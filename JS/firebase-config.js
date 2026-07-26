f66a5052167c825bf62e5c7c6c7009afc1418b21
OBO-WAN <157785208+OBO-WAN@users.noreply.github.com>
2026-07-26 08:38:32 +0200

    Enable Firebase App Check with reCAPTCHA Enterprise

--- JS/firebase-config.js
+++ JS/firebase-config.js
@@ -7,15 +7,23 @@ const firebaseConfig = {
   appId: "1:649210069998:web:e63de8aa2e76b60ffe84ff",
 };
 
+const recaptchaEnterpriseSiteKey = "6Lci5WUtAAAAAAIRK7JE9XEeByacGATn2wJPxLAm";
+
 let servicesPromise;
 
 export function getFirestoreServices() {
   if (!servicesPromise) {
     servicesPromise = Promise.all([
       import("https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js"),
+      import("https://www.gstatic.com/firebasejs/12.12.1/firebase-app-check.js"),
       import("https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js"),
-    ]).then(([firebaseApp, firestore]) => {
+    ]).then(([firebaseApp, appCheck, firestore]) => {
       const app = firebaseApp.initializeApp(firebaseConfig);
+
+      appCheck.initializeAppCheck(app, {
+        provider: new appCheck.ReCaptchaEnterpriseProvider(recaptchaEnterpriseSiteKey),
+        isTokenAutoRefreshEnabled: true,
+      });
 
       return {
         db: firestore.getFirestore(app),

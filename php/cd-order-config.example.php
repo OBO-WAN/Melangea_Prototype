3363fa658cfd17837ee6abb39aa938d2a6ae8e1d
OBO-WAN <157785208+OBO-WAN@users.noreply.github.com>
2026-07-29 15:34:20 +0200

    Simplify CD order configuration for manual payment

--- php/cd-order-config.example.php
+++ php/cd-order-config.example.php
@@ -5,8 +5,4 @@ declare(strict_types=1);
 return [
     'recipient_email' => 'info@melangea2.com',
     'from_email' => 'info@melangea2.com',
-    'bank_account_name' => 'Uwe Hanewald',
-    'bank_iban' => 'IBAN_HIER_EINTRAGEN',
-    'cd_price_eur' => '15',
-    'shipping_eur' => '3',
 ];
